6377e0a50847a7224af6be0b7c07fd3cd055a599
Matheus Tararam de Laurentys <matheus.laurentys@usp.br>
2022-01-13 09:23:07 -0500

    Update htmlResponses.js

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 // pull in the file system module
-const index = fs.readFileSync(`${__dirname} /../client/client.html`);
-const page2 = fs.readFileSync(`${__dirname} /../client/client2.html`);
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
